550494175255c1eaf18f4d00989becbcddf6a2dd
vanshika1711-cpu <vanshikadixit368@gmail.com>
2026-06-25 12:41:17 +0530

    Time: 0 ms (100%), Space: 47.7 MB (68.39%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,39 +1,44 @@
 class Solution {
     public int trap(int[] height) {
-       // 4 2 0 5 2 6 2 3
-        int len  = height.length ; 
-        int lmax [] = new int[len] ;
-        
-        // calculating lmax 
-        lmax[0] = height[0]; ;  
-        for(int i =  1 ; i < len  ; i++)
-        {
-            lmax[i] = Math.max(lmax[i-1], height[i]); 
-        }
-        // 0 , 4 , 4 , 4 ,5 , 5, 6 ,6 
-        int rmax[] = new int[len] ; 
-        //calculating rmax 
-        rmax[len-1] = height[len-1] ; 
-        for(int j =  len-2  ; j >= 0 ; j--)
-        {
-            rmax[j] = Math.max(rmax[j+1], height[j]);
-        }
-        //  6,6,6,6,6,3,3,0
 
-        
-    
-        int water[] = new int[len];
-        for(int i = 0; i < len; i++)
+        int left = 0;
+        int right = height.length - 1;
+
+        int leftMax = 0;
+        int rightMax = 0;
+
+        int water = 0;
+
+        while(left < right)
         {
-            water[i] = Math.min(lmax[i], rmax[i]) - height[i];
+            if(height[left] < height[right])
+            {
+                if(height[left] >= leftMax)
+                {
+                    leftMax = height[left];
+                }
+                else
+                {
+                    water += leftMax - height[left];
+                }
+
+                left++;
+            }
+            else
+            {
+                if(height[right] >= rightMax)
+                {
+                    rightMax = height[right];
+                }
+                else
+                {
+                    water += rightMax - height[right];
+                }
+
+                right--;
+            }
         }
 
-        int totalWater = 0;
-         for(int i = 0; i < len; i++)
-           {
-              totalWater += water[i];
-           }
-           return totalWater ; 
-
-        }
+        return water;
     }
+}
