948d50984bb51bbef4879b62c04068b4591956f1
vanshika1711-cpu <vanshikadixit368@gmail.com>
2026-06-25 23:31:29 +0530

    Time: 1 ms (59.74%), Space: 47.7 MB (56.37%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,43 +1,46 @@
 class Solution {
     public int trap(int[] height) {
-
-        int n = height.length;
-
-        // highest bar ka index
-        int peak = 0;
-
-        for(int i = 1; i < n; i++) {
-            if(height[i] > height[peak]) {
-                peak = i;
+        int len  = height.length  ; 
+        int lmax  = 0  ; 
+        int rmax  = 0 ; 
+        int water = 0; 
+        // for finding max height 
+        int maxheight = height[0] ; 
+        int index  = 0  ; 
+        // calculating max height builduing 
+        for(int i  = 1 ; i < len  ; i++)
+        {
+            if(height[i]>maxheight)
+            {
+                maxheight = height[i] ; 
+                index  =i ; 
             }
         }
+        //calculating left part 
+        for(int i = 0 ; i <index ; i++)
+        {
+          if(lmax > height[i])
+          {
+            water+=lmax-height[i] ; 
+          }
+          else 
+          {
+            lmax = height[i] ; 
+          }
+        }
+        //calculating right part
+        for(int i  = len-1 ; i>index ; i--)
+        {
+            if(rmax>height[i])
+            {
+                water+=rmax-height[i] ; 
 
-        int water = 0;
-
-        // Left part
-        int leftMax = height[0];
-
-        for(int i = 1; i < peak; i++) {
-
-            if(height[i] < leftMax) {
-                water += leftMax - height[i];
-            } else {
-                leftMax = height[i];
+            }
+            else 
+            {
+                rmax  = height[i] ; 
             }
         }
-
-        // Right part
-        int rightMax = height[n - 1];
-
-        for(int i = n - 2; i > peak; i--) {
-
-            if(height[i] < rightMax) {
-                water += rightMax - height[i];
-            } else {
-                rightMax = height[i];
-            }
-        }
-
-        return water;
+        return water ; 
     }
 }
