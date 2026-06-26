b171cef22af865204b383d59003d8451c54d71c1
vanshika1711-cpu <vanshikadixit368@gmail.com>
2026-06-26 07:04:03 +0530

    Time: 0 ms (100%), Space: 47.7 MB (68.24%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,46 +1,46 @@
 class Solution {
     public int trap(int[] height) {
-        int len  = height.length  ; 
-        int lmax  = 0  ; 
-        int rmax  = 0 ; 
-        int water = 0; 
-        // for finding max height 
-        int maxheight = height[0] ; 
-        int index  = 0  ; 
-        // calculating max height builduing 
-        for(int i  = 1 ; i < len  ; i++)
+      // 1 -> finding out the maximum of all builduings 
+      int len  = height.length  ; 
+      int maxheight  = height[0] ;
+      int index  = 0  ; 
+      for(int i = 0 ; i < len ; i++)
+      {
+        if(height[i]>maxheight)
         {
-            if(height[i]>maxheight)
-            {
-                maxheight = height[i] ; 
-                index  =i ; 
-            }
+            maxheight = height[i] ; 
+            index = i ; 
         }
-        //calculating left part 
-        for(int i = 0 ; i <index ; i++)
+      }
+      //left part 
+      int lmax  = 0 ; 
+      int water  = 0 ; 
+      for(int i  = 0 ; i < index ; i++)
+      {
+        if(lmax>height[i])
         {
-          if(lmax > height[i])
-          {
-            water+=lmax-height[i] ; 
-          }
-          else 
-          {
-            lmax = height[i] ; 
-          }
+           water+=lmax-height[i] ; 
+        } 
+        else 
+        {
+            lmax  = height[i] ; 
         }
-        //calculating right part
-        for(int i  = len-1 ; i>index ; i--)
-        {
-            if(rmax>height[i])
-            {
-                water+=rmax-height[i] ; 
 
-            }
-            else 
-            {
-                rmax  = height[i] ; 
-            }
+      }
+      // right part  
+      int rmax  =  0  ; 
+        for(int i  = len-1;i>index  ;  i--)
+      {
+        if(rmax>height[i])
+        {
+           water+=rmax-height[i] ; 
+        } 
+        else 
+        {
+            rmax  = height[i] ; 
         }
-        return water ; 
+
+      }
+      return water ; 
     }
 }
