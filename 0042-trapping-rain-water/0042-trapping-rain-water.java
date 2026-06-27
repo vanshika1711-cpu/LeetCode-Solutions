3a434cc4e831ceecfc9db18437052ba950183c45
vanshika1711-cpu <vanshikadixit368@gmail.com>
2026-06-27 21:33:38 +0530

    Time: 0 ms (100%), Space: 47.4 MB (95.2%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,46 +1,47 @@
 class Solution {
     public int trap(int[] height) {
-      // 1 -> finding out the maximum of all builduings 
-      int len  = height.length  ; 
-      int maxheight  = height[0] ;
-      int index  = 0  ; 
-      for(int i = 0 ; i < len ; i++)
-      {
-        if(height[i]>maxheight)
+        int len = height.length ; 
+        int lmax  = 0 ; 
+        int rmax  = 0  ; 
+        int water = 0 ; 
+        int maxheight = height[0]; 
+        int index = 0 ; 
+        // finding the maximum height 
+        for(int i = 0 ; i < len ; i++)
         {
-            maxheight = height[i] ; 
-            index = i ; 
+            if(height[i]>maxheight)
+            {
+                maxheight = height[i] ; 
+                index = i ; 
+            }
         }
-      }
-      //left part 
-      int lmax  = 0 ; 
-      int water  = 0 ; 
-      for(int i  = 0 ; i < index ; i++)
-      {
-        if(lmax>height[i])
+        // calculating left part 
+        int left[] = new int[len] ; 
+        left[0] = height[0] ; 
+        for(int i = 0  ; i < index ; i++)
         {
-           water+=lmax-height[i] ; 
-        } 
-        else 
-        {
-            lmax  = height[i] ; 
+           if(height[i]<lmax)
+           {
+            water+=lmax-height[i] ; 
+           }
+           else 
+           {
+            lmax = height[i] ; 
+           }
         }
+        // calculating right part 
+        for(int j  = len-1 ; j > index ; j--)
+        {
+             if(height[j]<rmax)
+           {
+            water+=rmax-height[j] ; 
+           }
+           else 
+           {
+            rmax = height[j] ; 
+           }
+        }
+        return water ; 
 
-      }
-      // right part  
-      int rmax  =  0  ; 
-        for(int i  = len-1;i>index  ;  i--)
-      {
-        if(rmax>height[i])
-        {
-           water+=rmax-height[i] ; 
-        } 
-        else 
-        {
-            rmax  = height[i] ; 
-        }
-
-      }
-      return water ; 
     }
 }
