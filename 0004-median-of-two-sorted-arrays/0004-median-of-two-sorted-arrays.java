0442825023b542f5e859e38dfc6f65954fc3d740
vanshika1711-cpu <vanshikadixit368@gmail.com>
2026-07-01 03:05:36 +0530

    Time: 1 ms (100%), Space: 48.7 MB (86.53%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,51 +1,44 @@
 class Solution {
-    public double findMedianSortedArrays(int[]nums1, int[] nums2) {
-        // arr1  = 1 ,2  
-        // arr2  = 3 , 4 
-        int len1  = nums1.length  ; 
-        int len2 = nums2.length  ; 
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int len1  = nums1.length ; 
+        int len2 = nums2.length ; 
         int nums3[] = new int[len1+len2] ; 
-        int len3 = nums3.length  ; 
-
-        // pointers 
-        int i  = 0 ; // for nums1
-        int j  = 0 ; // for nums2 
-        int k = 0 ; // for nums3
-
-        // loops to compare 
+        int len3  = nums3.length ; 
+        int i = 0 ; 
+        int j  = 0 ; 
+        int k  = 0 ; 
         while(i<len1 && j<len2)
         {
             if(nums1[i]<=nums2[j])
             {
                 nums3[k] = nums1[i] ; 
-                k++; 
                 i++; 
+                k++; 
             }
             else 
             {
-                 nums3[k] = nums2[j] ; 
-                 j++; 
-                 k++ ; 
+                nums3[k] = nums2[j] ; 
+                j++; 
+                k++;  
             }
         }
-        // if i or j is left or either one of the array is finished
-        while(i < len1)
+        // remain behind
+        while(i<len1)
         {
             nums3[k] = nums1[i] ; 
+            k++;
             i++; 
-            k++ ; 
         }
-         while(j < len2)
+        while(j<len2)
         {
             nums3[k] = nums2[j] ; 
-            j++; 
-            k++ ; 
+            k++;
+            j++ ;
         }
-        // finding out the median 
-        // checking the condition 
-        if (len3%2==0)
+        // finding median now 
+        if(len3%2==0)
         {
-            return (nums3[len3/2]+nums3[len3/2 -1])/2.0 ; 
+            return (nums3[len3/2]+nums3[len3/2 -1])/2.0; 
         }
         else 
         {
