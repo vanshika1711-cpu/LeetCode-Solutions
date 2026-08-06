44bc1df3d528bea3d847801a9b46548082b1ba10
vanshika1711-cpu <vanshikadixit368@gmail.com>
2026-08-06 21:41:12 +0530

    Time: 0 ms (100%), Space: 43.6 MB (8.69%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int len  = digits.length  ; 
+        for(int i = len-1 ; i >=0 ; i--)
+        {
+            if(digits[i]<9)
+            {
+                digits[i]++ ; 
+                return digits ; 
+            }
+            else 
+            {
+                digits[i] =  0 ; 
+            }
+        }
+        if(digits[0]==0)
+        {
+        int newArr[] = new int[len+1] ; 
+          newArr[0] = 1  ;  
+         return newArr ; 
+        }    
+        return digits ;       
+    }
+}
