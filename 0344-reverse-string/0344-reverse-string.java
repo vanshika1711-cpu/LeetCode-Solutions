2142d250aecf5139c306dc256552b49902a70f4c
vanshika1711-cpu <vanshikadixit368@gmail.com>
2026-09-06 00:03:46 +0530

    Time: 0 ms (100%), Space: 48.2 MB (55.22%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int len  =  s.length; 
+        int start =  0  ; 
+        int end  = len-1 ; 
+        while(start<=end)
+        {
+            char temp  = s[start] ; 
+            s[start] = s[end] ; 
+            s[end] = temp ; 
+            start++ ;
+            end-- ;
+        }
+
+    }
+}
