7d1a6ac34ef316827299bc05c440ea7061897893
vanshika1711-cpu <vanshikadixit368@gmail.com>
2026-09-08 09:21:56 +0530

    Time: 0 ms (100%), Space: 43.8 MB (63.57%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,28 +1,42 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-    int i  = m-1 ; 
-    int j  = n-1 ; 
-    int k  = m + n -1 ; 
-     while(i>=0 && j >= 0)
-     {
-        if(nums1[i]>nums2[j])
-        {
-            nums1[k] = nums1[i];  
-            i--;  
-            k-- ; 
-        }
-        else
-        {
-            nums1[k] = nums2[j] ; 
-            j-- ; 
-            k--;
-        }
-     }
-     // remaining 
-     while (j >= 0) {
-            nums1[k] = nums2[j];
-            j--;
-            k--;
-        }
+    int i =  0  ; 
+    int j  =  0;
+    int k  = 0  ; 
+    int len  = m+  n  ;  
+    int nums3[] = new int[len] ; 
+    
+    while(i<m && j<n)
+    {
+      if(nums1[i]<nums2[j])
+      {
+        nums3[k] = nums1[i]  ; 
+        i++ ;
+        k++;
+      }
+      else {
+        nums3[k] = nums2[j] ; 
+        j++ ;
+        k++ ;
+      }
+    }
+    while(i<m)
+    {
+        nums3[k] = nums1[i] ; 
+        i++; 
+        k++ ;
+    }
+    while(j<n)
+    {
+        nums3[k] = nums2[j] ; 
+        j++ ;
+        k++ ;
+    }
+    // return in nums1 
+    for(int x =0 ; x < len  ;x++ )
+    {
+        nums1[x] =  nums3[x] ; 
+
+    }
     }
 }
