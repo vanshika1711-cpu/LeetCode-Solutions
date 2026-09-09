e789f1d2773e7ba8f8ae23ffed80b68898cec718
vanshika1711-cpu <vanshikadixit368@gmail.com>
2026-09-09 23:08:13 +0530

    Time: 0 ms (100%), Space: 43.7 MB (8.37%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,30 +1,34 @@
 class Solution {
     public void sortColors(int[] nums) {
-     int len =  nums.length ; 
-     int low  =0   ; 
-     int mid  = 0   ; 
-     int high = len-1 ;  
-     for(int i  = 0 ; i < len  ; i++)
-     {
+    // dutch national flag algorithm 
+
+    int len  =  nums.length ;  
+    int low  = 0 ; 
+    int mid  = 0  ; 
+    int high  =  len-1  ;  
+    for(int i  = 0  ;  i < len  ; i++)
+    {
         if(nums[mid]==0)
         {
-          int temp = nums[mid] ; 
-          nums[mid] = nums[low] ; 
-          nums[low] = temp ; 
-          mid++;
-          low++ ;
+            int temp  =  nums[mid] ; 
+            nums[mid] = nums[low] ; 
+            nums[low] = temp  ; 
+            mid++;  
+            low ++ ; 
         }
         else if (nums[mid]==1)
         {
-            mid++ ;
+            mid++ ; 
         }
-        else {
-            int temp = nums[mid] ; 
-            nums[mid] = nums[high] ; 
-            nums[high]  = temp ; 
-            high-- ;
+        else 
+        {
+            int temp  = nums[mid] ; 
+            nums[mid] = nums[high];   
+            nums[high] = temp ; 
+            high-- ; 
+             
         }
-     }
+    }
 
     }
 }
