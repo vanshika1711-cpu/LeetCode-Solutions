7f1dd1a284b04a3d8b91c6018a207a40e2527318
vanshika1711-cpu <vanshikadixit368@gmail.com>
2026-09-15 13:19:58 +0530

    Time: 20 ms (81.64%), Space: 56.8 MB (9.1%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -1,26 +1,24 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
-        int len  =  people.length ;  
-        Arrays.sort(people) ; 
-        int light  = 0  ; 
-        int heavy  = len-1 ; 
-        int boat  = 0  ;   
-        while(light<=heavy)
+    int len  = people.length ; 
+    int light  =  0 ; 
+    Arrays.sort(people) ; 
+    int heavy  = len - 1 ;
+    int boat  = 0   ; 
+    while(light<=heavy)
+    {
+        if(people[light]+people[heavy]<=limit)
         {
-            if(people[light]+people[heavy]<=limit)
-            {
-                boat++ ; 
-                light++ ;
-                heavy-- ; 
-            }
-            else 
-            {
-                boat++ ;
-                heavy-- ;
-            }
-
+           boat++ ;
+           light++ ;
+           heavy-- ;
         }
-        
-        return boat ; 
+        else 
+        {
+            boat++ ;
+            heavy-- ;
+        }
+    }
+    return boat  ; 
     }
 }
